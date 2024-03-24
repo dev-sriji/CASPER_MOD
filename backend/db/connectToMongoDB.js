d6d067ba5b4ea3f69707bc57e0c8cc6e683f6b24
Srijith <srijith@kali.kali>
2024-03-24 10:55:42 +0530

    before adding qrcode to frontend

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import Config from "../config.js";
+import * as colors from "colors";
 
 const connectToMongoDB = async () => { 
     try {
         await mongoose.connect(Config.MONGO_URI)
-        console.log("Connected To Mongo With Status Code : 200")
+        console.log("Connected To Mongo With Status Code : 200".bgGreen)
     } catch (error) {
-        console.log("Error Connecting To MongoDB...\n",error)
+        console.log("Error Connecting To MongoDB...\n".bgRed,error)
     }
 };
 
--- backend/socketio/socket.js
+++ backend/socketio/socket.js
@@ -5,10 +5,11 @@ import http from 'http';
 import express from 'express'
 import Config from "../config.js";
 import cors from "cors";
+import * as colors from "colors";
 
 const PORT = Config.PORT || 8080;
 const app = express();
-const server = http.createServer(app); // Create HTTP server using Express app
+const server = http.createServer(app); 
 
 app.use(cors()); // Apply CORS middleware here
 
@@ -20,7 +21,7 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log("ok 9340", socket.id)
+    console.log("New Socket Created".red, JSON.stringify(socket.id, null, 2).italic.yellow)
     
     socket.on("disconnect", () => {
         // Handle disconnect
