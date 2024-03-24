1c2767c778ad05a6d1e517beae8cd09554023b5a
Srijith <srijith@kali.kali>
2024-03-24 15:15:43 +0530

    Console Colours & Intro

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 //index.js
 
 import express from "express";
-import * as colors from 'colors'
+import * as colors from "colors";
 import {
   useMultiFileAuthState,
   makeWASocket,
@@ -10,12 +10,11 @@ import {
 import { connectToMongoDB } from "./db/connectToMongoDB.js";
 import Config from "./config.js";
 import cors from "cors";
-import {app, io, server} from './socketio/socket.js'
+import { app, io, server } from "./socketio/socket.js";
 import path from "path";
 const __dirname = path.resolve();
 const PORT = Config.PORT || 8080;
 import cfonts from "cfonts";
-
 
 app.use(express.json());
 app.use(cors());
@@ -29,13 +28,14 @@ app.get("/app", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-
-
 import Message from "./models/message-model.js";
 
 await server.listen(PORT, () => {
   connectToMongoDB();
-  console.log("Server Is Running On ", `http://localhost:${PORT}`.blue.underline.bold);
+  console.log(
+    "Server Is Running On ",
+    `http://localhost:${PORT}`.blue.underline.bold
+  );
 });
 
 // Function to connect to WhatsApp
@@ -71,63 +71,77 @@ async function connectToWhatsApp() {
 
   // Listener for 'messages.upsert': { messages: WAMessage[], type: MessageUpsertType }
   sock.ev.on("messages.upsert", ({ messages }) => {
-
     const m = {
-        name: messages[0]?.pushName?.replace(/\n/g, "")?.trim() || "",
-        chat: messages[0]?.key?.remoteJid || "",
-        sender: messages[0]?.key?.remoteJid?.endsWith("@s.whatsapp.net")
+      name: messages[0]?.pushName?.replace(/\n/g, "")?.trim() || "",
+      chat: messages[0]?.key?.remoteJid || "",
+      sender: messages[0]?.key?.remoteJid?.endsWith("@s.whatsapp.net")
         ? messages[0]?.key?.remoteJid
         : messages[0]?.key?.participant,
-        isGroup: messages[0]?.key?.remoteJid?.endsWith("@g.us") ? true : false,
-        isDm: messages[0]?.key?.remoteJid?.endsWith("@s.whatsapp.net")
+      isGroup: messages[0]?.key?.remoteJid?.endsWith("@g.us") ? true : false,
+      isDm: messages[0]?.key?.remoteJid?.endsWith("@s.whatsapp.net")
         ? true
         : false,
-        text:
+      text:
         messages[0]?.message?.conversation ||
         messages[0]?.message?.extendedTextMessage?.text,
-        key: messages[0]?.key?.id || "",
-        fromMe: messages[0]?.key?.fromMe,
-        definedType: (messages[0]?.message?.conversation ||
-          messages[0]?.message?.extendedTextMessage?.text) ? 'textMessage' : messages[0]?.message?.stickerMessage ? 'stickerMessage' : messages[0]?.message?.imageMessage ? 'imageMessage' : messages[0]?.message?.videoMessage ? 'videoMessage' : messages[0]?.message?.audioMessage ? 'audioMessage' : messages[0]?.message?.editedMessage ? 'editedMessage' : messages[0]?.message?.viewOnceMessage ? 'viewOnceMessage' : messages[0]?.message?.viewOnceMessageV2 ? 'viewOnceMessageV2' : undefined,
-        quoted: {
-            log:
-            messages[0].message?.extendedTextMessage?.contextInfo ||
-            messages[0].message?.extendedTextMessage?.contextInfo,
-            text:
-            messages[0].message?.extendedTextMessage?.contextInfo?.quotedMessage
+      key: messages[0]?.key?.id || "",
+      fromMe: messages[0]?.key?.fromMe,
+      definedType:
+        messages[0]?.message?.conversation ||
+        messages[0]?.message?.extendedTextMessage?.text
+          ? "textMessage"
+          : messages[0]?.message?.stickerMessage
+          ? "stickerMessage"
+          : messages[0]?.message?.imageMessage
+          ? "imageMessage"
+          : messages[0]?.message?.videoMessage
+          ? "videoMessage"
+          : messages[0]?.message?.audioMessage
+          ? "audioMessage"
+          : messages[0]?.message?.editedMessage
+          ? "editedMessage"
+          : messages[0]?.message?.viewOnceMessage
+          ? "viewOnceMessage"
+          : messages[0]?.message?.viewOnceMessageV2
+          ? "viewOnceMessageV2"
+          : undefined,
+      quoted: {
+        log:
+          messages[0].message?.extendedTextMessage?.contextInfo ||
+          messages[0].message?.extendedTextMessage?.contextInfo,
+        text:
+          messages[0].message?.extendedTextMessage?.contextInfo?.quotedMessage
             ?.conversation ||
-            messages[0].message?.extendedTextMessage?.contextInfo?.text ||
-            "",
-            sender:
-            messages[0].message?.extendedTextMessage?.contextInfo?.participant ||
-            "",
-            tagged:
-            messages[0].message?.extendedTextMessage?.contextInfo?.mentionedJid ||
-            "",
-        },
-        replyingTo: messages[0]?.key?.remoteJid?.endsWith("@s.whatsapp.net")
+          messages[0].message?.extendedTextMessage?.contextInfo?.text ||
+          "",
+        sender:
+          messages[0].message?.extendedTextMessage?.contextInfo?.participant ||
+          "",
+        tagged:
+          messages[0].message?.extendedTextMessage?.contextInfo?.mentionedJid ||
+          "",
+      },
+      replyingTo: messages[0]?.key?.remoteJid?.endsWith("@s.whatsapp.net")
         ? messages[0]?.message?.extendedTextMessage?.contextInfo?.quotedMessage
-        ?.conversation ||
-        messages[0].message?.extendedTextMessage?.contextInfo?.text ||
-        global?.db?.data?.users[messages[0]?.key?.remoteJid]?.lastReply ||
-        ""
+            ?.conversation ||
+          messages[0].message?.extendedTextMessage?.contextInfo?.text ||
+          global?.db?.data?.users[messages[0]?.key?.remoteJid]?.lastReply ||
+          ""
         : messages[0]?.message?.extendedTextMessage?.contextInfo?.quotedMessage
-        ?.conversation ||
-        messages[0].message?.extendedTextMessage?.contextInfo?.text ||
-        false,
-        itself: messages[0],
+            ?.conversation ||
+          messages[0].message?.extendedTextMessage?.contextInfo?.text ||
+          false,
+      itself: messages[0],
     };
     // console.log(m);
     handleMessage(m);
-});
+  });
 
-
-
-app.post("/sendMessage", (req, res) => {
+  app.post("/sendMessage", (req, res) => {
     try {
-        const chat = req.body.chat || Config.MODS;
-        const text = req.body.payload.text || "";
-        sock.sendMessage(chat || Config.MODS, { text: text });
+      const chat = req.body.chat || Config.MODS;
+      const text = req.body.payload.text || "";
+      sock.sendMessage(chat || Config.MODS, { text: text });
       // console.log(req.body)
       res.status(200).json({ reciever: chat, text: text });
       // res.status(200).json({ data: "everything ok" });
@@ -143,8 +157,9 @@ app.post("/sendMessage", (req, res) => {
         return res.status(400).json({ error: "chat parameter is missing" });
       }
 
-      const mess = await Message.find({ chat: requestedChat })
-        .sort({ updatedAt: 1 });
+      const mess = await Message.find({ chat: requestedChat }).sort({
+        updatedAt: 1,
+      });
 
       if (!mess || mess.length === 0) {
         return res.status(200).json({});
@@ -167,10 +182,10 @@ app.post("/sendMessage", (req, res) => {
         {
           $group: {
             _id: "$chat",
-            name: { $first: "$name" }, 
-            chat: { $first: "$chat" }, 
-            isGroup: { $first: "$isGroup" }, 
-            updatedAt: {$first: "$updatedAt"},
+            name: { $first: "$name" },
+            chat: { $first: "$chat" },
+            isGroup: { $first: "$isGroup" },
+            updatedAt: { $first: "$updatedAt" },
           },
         },
         {
@@ -179,7 +194,7 @@ app.post("/sendMessage", (req, res) => {
             name: 1,
             chat: 1,
             isGroup: 1,
-            updatedAt:1,
+            updatedAt: 1,
           },
         },
       ]);
@@ -193,75 +208,119 @@ app.post("/sendMessage", (req, res) => {
   async function handleMessage(m) {
     // console.log(m)
     if (m.isGroup) return;
-    if(m.definedType === undefined) return console.log('UNDEFINED Message Type Found... LOGGING MESSAGE OBJ :\n'.underline.red, JSON.stringify(m.itself, null, 2).italic.yellow);
-    const separator = "--------------------------------------------------------";
+    if (m.definedType === undefined)
+      return console.log(
+        "UNDEFINED Message Type Found... LOGGING MESSAGE OBJ :\n".underline.red,
+        JSON.stringify(m.itself, null, 2).italic.yellow
+      );
+    const separator =
+      "--------------------------------------------------------";
     const arrow = "➜";
     const replyEmoji = "💬";
     const senderEmoji = "👤";
     const groupEmoji = "👥";
-    
+
     // Log a text message with enhanced design
     console.log(separator);
     console.log(
-      `📩 A text message received from ${senderEmoji} ${m.sender?.green} in chat ${m.chat?.green} 📩\n`.bold
-      );
-      console.log("Name:".magenta, m.name?.red.bold);
-      console.log("Chat:".magenta, m.chat?.yellow);
-      console.log("Sender:".magenta, `${senderEmoji} ${m.sender?.yellow.italic}`);
-      console.log(
-        "Is Group:".magenta,
-        `${m.isGroup ? `${groupEmoji} Yes` : "No"}`.yellow
-        );
-        console.log("Is DM:".magenta, `${m.isDm ? "Yes" : "No"}`.yellow);
-        console.log("Message Type:".magenta, m.definedType?.yellow);
-        console.log("Text:".magenta, m.text?.cyan.bold || 'undefined'.red);
-        console.log("Key:".magenta, m.key?.yellow);
-        console.log("From Me:".magenta, `${m.fromMe ? "Yes" : "No"}`.yellow);
-        console.log("Quoted :".magenta, m.quoted ? "true".green : "false".red);
-        console.log(
-          m.quoted ? 
-          `Quoted Sender: ${senderEmoji} ${m.quoted.sender}`.magenta.yellow : ''
-          );
+      `📩 A text message received from ${senderEmoji} ${m.sender?.green} in chat ${m.chat?.green} 📩\n`
+        .bold
+    );
+    console.log("Name:".magenta, m.name?.red.bold);
+    console.log("Chat:".magenta, m.chat?.yellow);
+    console.log("Sender:".magenta, `${senderEmoji} ${m.sender?.yellow.italic}`);
+    console.log(
+      "Is Group:".magenta,
+      `${m.isGroup ? `${groupEmoji} Yes` : "No"}`.yellow
+    );
+    console.log("Is DM:".magenta, `${m.isDm ? "Yes" : "No"}`.yellow);
+    console.log("Message Type:".magenta, m.definedType?.yellow);
+    console.log("Text:".magenta, m.text?.cyan.bold || "undefined".red);
+    console.log("Key:".magenta, m.key?.yellow);
+    console.log("From Me:".magenta, `${m.fromMe ? "Yes" : "No"}`.yellow);
+    console.log("Quoted :".magenta, m.quoted ? "true".green : "false".red);
+    console.log(
+      m.quoted
+        ? `Quoted Sender: ${senderEmoji} ${m.quoted.sender}`.magenta.yellow
+        : ""
+    );
 
-          console.log("Tagged:".magenta, m.quoted?.tagged);
-          
-          // Design for Replying To section
-          if (m.replyingTo) {
-            console.log(`\t╰┈➤ ${replyEmoji} Replying To:`.bgYellow.black);
-            console.log(`\t${arrow} ❝ ${m.replyingTo?.cyan}❞`);
-          }
-          
-          console.log(separator);
-          await saveMessage(m);         
-          io.emit('recievedMessage',m)
-        }
-        const saveMessage = async (messageData) => {
-          try {
-            const message = new Message(messageData);
-            await message.save();
-            console.log("Message saved successfully :)".blue.italic.bold);
-          } catch (error) {
-            console.log("Error saving message to database:", error);
-          }
-        };
+    console.log("Tagged:".magenta, m.quoted?.tagged);
 
+    // Design for Replying To section
+    if (m.replyingTo) {
+      console.log(`\t╰┈➤ ${replyEmoji} Replying To:`.bgYellow.black);
+      console.log(`\t${arrow} ❝ ${m.replyingTo?.cyan}❞`);
+    }
 
-        cfonts.say('Welcome|To|Casper|Reloaded!', {
-          font: '3d',              // define the font face
-          align: 'left',              // define text alignment
-          colors: ['redBright','cyan'],         // define all colors
-          background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-          letterSpacing: 1,           // define letter spacing
-          lineHeight: 1,              // define the line height
-          space: true,                // define if the output text should have empty lines on top and on the bottom
-          maxLength: '0',             // define how many character can be on one line
-          gradient: false,            // define your two gradient colors
-          independentGradient: false, // define if you want to recalculate the gradient for each new line
-          transitionGradient: false,  // define if this is a transition between colors directly
-          env: 'node'                 // define the environment cfonts is being executed in
-        });
-      }
-      
-      // Connect to WhatsApp when the server starts
-      connectToWhatsApp();
-      
+    console.log(separator);
+    await saveMessage(m);
+    io.emit("recievedMessage", m);
+  }
+  const saveMessage = async (messageData) => {
+    try {
+      const message = new Message(messageData);
+      await message.save();
+      console.log("Message saved successfully :)".blue.italic.bold);
+    } catch (error) {
+      console.log("Error saving message to database:", error);
+    }
+  };
+
+  const randomFont = [
+    "console",
+    "block",
+    "simpleBlock",
+    "simple",
+    "3d",
+    "simple3d",
+    "chrome",
+    "huge",
+    "shade",
+    "slick",
+    "grid",
+    "pallet",
+  ];
+  const validColors = [
+    "black",
+    "red",
+    "green",
+    "yellow",
+    "blue",
+    "magenta",
+    "cyan",
+    "white",
+    "gray",
+    "#3456ff",
+    "#f80",
+  ];
+  const additionalColors = ["#ff5733", "#33ff57", "#5733ff"];
+
+  // Combine valid colors with additional colors
+  const allColors = validColors.concat(additionalColors);
+
+  // Randomly select a font style and gradient colors
+  const selectedFont =
+    randomFont[Math.floor(Math.random() * randomFont.length)];
+  const selectedColor1 =
+    allColors[Math.floor(Math.random() * allColors.length)];
+  const selectedColor2 =
+    allColors[Math.floor(Math.random() * allColors.length)];
+
+  cfonts.say("Welcome|To|Casper|Reloaded!", {
+    font: selectedFont,
+    align: "left",
+    colors: ["white"],
+    background: "transparent",
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: true,
+    maxLength: "0",
+    gradient: [selectedColor1, selectedColor2],
+    independentGradient: false,
+    transitionGradient: false,
+    env: "node",
+  });
+}
+
+connectToWhatsApp();
