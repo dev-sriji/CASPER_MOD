73c6b123e1f725fde3e247e70a10d538a1941a5a
Srijith <srijith@kali.kali>
2024-03-31 10:32:40 +0530

    Final Commit For Whatsapp-Clone_Single User

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 const connectToMongoDB = async () => { 
     try {
-        await mongoose.connect(Config.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected To Mongo With Status Code : 200")
     } catch (error) {
         console.log("Error Connecting To MongoDB...\n",error)
